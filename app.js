00ea101a21d7592f3d4733f7631207fb19ed3d4f
Suzuki Shunsuke <shunsuke@shunsuke-ThinkPad-Edge-E430.(none)>
2013-02-14 17:16:58 +0900

    aaa

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ var app = express();
 app.use(partial());
 
 app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
+  app.set('port', process.env.PORT || 5000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'ejs');
   app.use(express.favicon());
